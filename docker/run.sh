1ecf65f0f90d0fe361b14bb435b858d974a227d5
gar <garlandk@gmail.com>
2016-06-03 17:21:39 -0700

    adding login True flag

--- docker/run.sh
+++ docker/run.sh
@@ -6,8 +6,8 @@ echo "root:${PASSWORD}" | chpasswd
 if [ -z ${PORT} ]
 then
   echo "Starting on default port: 57575"
-  /opt/app/butterfly.server.py --unsecure --host=0.0.0.0
+  /opt/app/butterfly.server.py --unsecure --host=0.0.0.0 --login True
 else
   echo "Starting on port: ${PORT}"
-  /opt/app/butterfly.server.py --unsecure --host=0.0.0.0 --port=${PORT}
+  /opt/app/butterfly.server.py --unsecure --host=0.0.0.0 --port=${PORT} --login True
 fi
